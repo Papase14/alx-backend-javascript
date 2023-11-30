af608c69a5148371406d0f784dc60e2a97ed680a
Tshupane Morake <tshupane@outlook.com>
2023-11-30 22:35:07 +0200

    2. A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
+
+
+    get name() {
+        return this._name; // get name
+    }
+
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
+        this._name = value; // set name
+    }
+
+    get length() {
+        return this._length; //get length
+    }
+
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Length must be a number');
+        }
+        this._length = value;
+    }
+
+    get students() {
+        return this._students; //get names //get names
+    }
+
+    set students(value) {
+        if (!(value instanceof Array)) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        if (!value.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
+        }
+        this._students = value; //set names
+    }
+}
